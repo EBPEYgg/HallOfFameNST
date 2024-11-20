542c26fd9a41e88f881f7903508d1bab20fa23d0
Алексей <com.twin@yandex.ru>
2024-11-21 01:43:33 +0700

    feature: add logging

--- src/HallOfFameNST/Program.cs
+++ src/HallOfFameNST/Program.cs
@@ -1,25 +1,47 @@
 using HallOfFameNST.Model.Data;
 using Microsoft.EntityFrameworkCore;
+using NLog;
+using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(args);
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("Initializing application");
 
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<HallOfFameNSTContext>
-    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    var builder = WebApplication.CreateBuilder(args);
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
-app.Run();
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddDbContext<HallOfFameNSTContext>
+        (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+    var app = builder.Build();
+
+    app.Logger.LogInformation("Starting the app");
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
+catch (Exception ex)
+{
+    logger.Error("Stopped program because of exception. ", ex);
+    throw;
+}
+finally
+{
+    NLog.LogManager.Shutdown();
+}
