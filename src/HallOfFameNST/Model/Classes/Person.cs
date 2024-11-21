4491444dd4c638231d9664afb46d1091740cd354
Алексей <com.twin@yandex.ru>
2024-11-21 19:03:41 +0700

    feature: add model-level validation attributes

--- src/HallOfFameNST/Model/Classes/Person.cs
+++ src/HallOfFameNST/Model/Classes/Person.cs
@@ -1,11 +1,17 @@
-﻿namespace HallOfFameNST.Model.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HallOfFameNST.Model.Classes
 {
     public class Person
     {
         public long Id { get; set; }
 
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot be longer than 100 characters.")]
         public string Name { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "DisplayName is required.")]
+        [StringLength(100, ErrorMessage = "DisplayName cannot be longer than 100 characters.")]
         public string DisplayName { get; set; } = string.Empty;
 
         public virtual ICollection<Skill> Skills { get; set; } = [];
